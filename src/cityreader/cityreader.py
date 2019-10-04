74bad848d0d17978da398095d7652a8e0ed180ff
abidex4yemi <abidex4yemi.com>
2019-10-04 18:11:23 +0100

    Add stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -57,8 +57,8 @@ def cityreader(cities=[]):
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-# for city in cities:
-# print(f"City name: {city.name} latitude: {city.lat} longitude: {city.lon}")
+for city in cities:
+    print(f"City name: {city.name} latitude: {city.lat} longitude: {city.lon}")
 
 # STRETCH GOAL!
 #
@@ -91,22 +91,37 @@ cityreader(cities)
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
 
-# TODO Get latitude and longitude values from the user
-
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
     # within will hold the cities that fall within the specified region
     within = []
-    for city in cities:
-        if (float(lat1) <= float(city.lat) <= float(lon1) or
-                float(lat2) <= float(city.lat) <= float(lon2)):
-            within.append(cities)
+    latitude = [float(lat1), float(lat2)]
+    longitude = [float(lon1), float(lon2)]
 
-    # TODO Ensure that the lat and lon valuse are all floats
-    # Go through each city and check to see if it falls within
-    # the specified coordinates.
+    # for index in range(0, 2):
+    #     print(latitude[index])
+    #     if latitude[index] < latitude[index + 1]:
+    #         latitude[index] = latitude[index + 1]
+    #         longitude[index] = longitude[index + 1]
+
+    if latitude[0] < latitude[1]:
+        latitude[0] = latitude[1]
+        longitude[0] = longitude[1]
+    else:
+        latitude[1] = latitude[0]
+        longitude[1] = longitude[0]
+
+    for city in cities:
+        if ((city.lat >= latitude[0] and city.lat <= latitude[1]) and
+                (city.lon >= longitude[0] and city.lon <= longitude[1])):
+            within.append((city))
 
     return within
 
 
-print(cityreader_stretch(45, -100, 32, -120, cities))
+# user_input = input("Enter coordinate format: 45 -100 32 -120 => \n")
+# coordinates = user_input.split(" ")
+
+# print(cityreader_stretch(
+#     coordinates[0], coordinates[2], coordinates[1], coordinates[3], cities))
+# print(cityreader_stretch(45, -100, 32, -120, cities))
